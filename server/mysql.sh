8f6589e88ec916dd522ea416041707e1e22b5280
Amr Abed <amrabed@vt.edu>
2016-11-09 17:43:13 -0500

    Use docker-compose to start/stop servers

--- server/mysql.sh
+++
@@ -1,3 +0,0 @@
-docker stop mysql
-docker rm mysql
-docker run -d --name mysql -p 3306:3306 -v ~amrabed/metrolab/data/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=P@ssw0rd mysql
--- server/php.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#svn checkout https://github.com/docker-library/php/trunk/7.0/apache
-#cd apache
-#docker build -t php .
-
-docker stop php
-docker rm php
-docker run -d -p 8080:80 --name php --link mysql -v ~amrabed/metrolab/server/web:/var/www/html php
