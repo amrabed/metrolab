4fed070ecca8367ab13d7cf31c2e81a599b922e7
Amr Abed <amraabed@gmail.com>
2016-12-05 14:00:55 -0500

    Upgrade to Bootstrap 4

--- server/web/index.php
+++ server/web/index.php
@@ -1,31 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="refresh" content="1200">
-	<meta charset="utf-8">
-	<!--meta http-equiv="X-UA-Compatible" content="IE=edge"-->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="1200">
+    <meta charset="utf-8">
+    <!--meta http-equiv="X-UA-Compatible" content="IE=edge"-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>MetroLab Sensor Network Data</title>
+    <title>MetroLab Sensor Network Data</title>
 
-	<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-	<link href="style/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"
+          integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
 </head>
 <body>
-	<nav class="navbar navbar-inverse">
-		<div class="navbar-header">
-			<a class="navbar-brand" href="#">MetroLab Sensor Network Data</a>
-		</div>
-	</nav>
-	<div class="container">
-		<div id="graph" class="container" style="height: 1000px;"></div>
-		<div class="row"><?php include 'table.php';?></div>
-
-		<!-- BOOTSTRAP SCRIPTS -->
-		<script src="graph.js"></script>
-		<script
-			src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="style/js/bootstrap.min.js"></script>
-	</div>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="#">MetroLab Sensor Network Data</a>
+    </div>
+</nav>
+<div class="container">
+    <div id="graph" class="container" style="height: 1000px;"></div>
+    <div class="row"><?php include 'table.php'; ?></div>
+</div>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<script src="graph.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"
+        integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"
+        integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"
+        integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK"
+        crossorigin="anonymous"></script>
 </body>
 </html>
