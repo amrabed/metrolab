97a423541adf8f243886490165173214abd0bbdc
Amr Abed <amraabed@gmail.com>
2016-12-15 21:13:18 -0500

    Add search options

--- server/web/database.php
+++ server/web/database.php
@@ -4,107 +4,117 @@ require_once 'config.php';
 /*
  * Database operations
  */
+
 class Database
 {
-  /*
-   * Connect to database
-   */
-  function connect ()
-  {
-    $db = new mysqli (HOST, USER, PASSWORD, DATABASE, PORT /* Update values in config.php */);
-    if ($db->connect_errno)
+    /*
+     * Connect to database
+     */
+    function connect()
     {
-      die ("Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error);
-    }
-    return $db;
-  }
-
-  /*
-   * Read from databse into CSV file
-   */
-  function readCSV ($query)
-  {
-    $db = self::connect ();
-    if ( $result = $db->query ($query))
-    {
-      $csv = "timestamp,co,ozone,temperature,pressure,humidity\n";
-      foreach ( $result as $row )
-      {
-        echo '<tr>';
-        foreach ( $row as $item )
-        {
-          echo '<td>' . $item . '</td>';
-          $csv .= $item . ",";
+        $db = new mysqli (HOST, USER, PASSWORD, DATABASE, PORT /* Update values in config.php */);
+        if ($db->connect_errno) {
+            die ("Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error);
         }
-        echo '</tr>';
-        $csv = substr ($csv, 0 , -1) . "\n";
-      }
-      $result -> close ();
+        return $db;
     }
-    $db -> close ();
-    $file = fopen ('data.csv', 'w');
-    fwrite ($file, $csv);
-    fclose ($file);
-  }
 
-  /*
-   * Read from database into JSON array (Function not currently used)
-   */
-  function read (/* $query */)
-  {
-    $query = "SELECT * FROM Blacksburg where datediff(now(), timestamp) <= 7 ORDER BY timestamp DESC";
-    $db = self::connect ();
-    if ( $result = $db->query ($query))
+    /*
+     * Read from databse into CSV file
+     */
+    function readCSV($query)
     {
-      $array = json_encode ( $result-> fetch_all ( MYSQLI_ASSOC ));
-      $result -> close ();
+        $db = self::connect();
+        if ($result = $db->query($query)) {
+            $csv = "timestamp,co,ozone,temperature,pressure,humidity\n";
+            foreach ($result as $row) {
+                echo '<tr>';
+                foreach ($row as $item) {
+                    echo '<td>' . $item . '</td>';
+                    $csv .= $item . ",";
+                }
+                echo '</tr>';
+                $csv = substr($csv, 0, -1) . "\n";
+            }
+            $result->close();
+        }
+        $db->close();
+        $file = fopen('data.csv', 'w');
+        fwrite($file, $csv);
+        fclose($file);
     }
-    $db -> close ();
-    return $array;
-  }
 
-  /*
-   * Insert new values into database
-   */
-  function insert ($table, $fields, $values)
-  {
-    // Table, fields, and values received as strings from Raspberry Pi
-    $query = "INSERT INTO " . $table . " " . $fields . " VALUES " . $values;
-    $db = self::connect ();
-    if ($result = $db->query ($query))
+    /*
+     * Query the database
+     */
+    function query($query)
     {
-      $result -> close ();
+        $db = self::connect();
+        if ($result = $db->query($query)) {
+            $array = $result->fetch_all();
+            $result->close();
+        }
+        $db->close();
+        return $array;
     }
-    else
+
+    /**
+     * Get info of database tables
+     *
+     * @return array of database tables
+     */
+    function getTables()
     {
-      die ("Insert operation failed: " . $db -> error);
+        return self::query("show tables");
     }
-    $db -> close ();
-  }
+
+    /*
+     * Read from database into JSON array (Function not currently used)
+     */
+    function read($query)
+    {
+        $db = self::connect();
+        if ($result = $db->query($query)) {
+            $array = json_encode($result->fetch_all(MYSQLI_ASSOC));
+            $result->close();
+        }
+        $db->close();
+        return $array;
+    }
+
+    /*
+     * Insert new values into database
+     */
+    function insert($table, $fields, $values)
+    {
+        // Table, fields, and values received as strings from Raspberry Pi
+        $query = "INSERT INTO " . $table . " " . $fields . " VALUES " . $values;
+        $db = self::connect();
+        if ($result = $db->query($query)) {
+            $result->close();
+        } else {
+            die ("Insert operation failed: " . $db->error);
+        }
+        $db->close();
+    }
 
 
-  /*
-   * Execute a query
-   */
-  function execute ($query)
-  {
-    try
+    /*
+     * Execute a query
+     */
+    function execute($query)
     {
-      $db = self::connect ();
-      if ($result = $db->query ($query))
-      {
-        $result -> close ();
-      }
+        try {
+            $db = self::connect();
+            if ($result = $db->query($query)) {
+                $result->close();
+            }
+        } catch (Exception $e) {
+            echo 'Operation failed: ', $e->getMessage(), "\n";
+        } finally {
+            $db->close();
+        }
     }
-    catch (Exception $e)
-    {
-      echo 'Operation failed: ', $e->getMessage(), "\n";
-    }
-    finally
-    {
-      $db -> close ();
-    }
-  }
 }
 
 
--- server/web/index.php
+++ server/web/index.php
@@ -1,15 +1,35 @@
+<?php
+require_once 'database.php';
+$db = new Database;
+
+if (isset($_GET['device']) && isset($_GET['start']) && isset($_GET['end'])) {
+    $device = $_REQUEST['device'];
+    $start = $_REQUEST['start'];
+    $end = $_REQUEST['end'];
+    $query = "SELECT * FROM " . $device . " WHERE timestamp BETWEEN '" . $start . "' AND '" . $end . "'";
+} else {
+    $query = "SELECT * FROM Blacksburg WHERE datediff(now(), timestamp) <= 7";
+}
+$query .= " ORDER BY timestamp DESC";
+
+function _date($shift = 0)
+{
+    $d = new DateTime("now", new DateTimeZone("EST"));
+    $d->sub(new DateInterval('P' . $shift . 'D'));
+    return $d->format('Y-m-d\TH:i');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta http-equiv="refresh" content="1200">
     <meta charset="utf-8">
-    <!--meta http-equiv="X-UA-Compatible" content="IE=edge"-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>MetroLab Sensor Network Data</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"
-          integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css">
 </head>
 <body>
 <nav class="navbar navbar-default navbar-fixed-top">
@@ -17,20 +37,51 @@
         <a class="navbar-brand" href="#">MetroLab Sensor Network Data</a>
     </div>
 </nav>
-<div class="container">
-    <div id="graph" class="container" style="height: 1000px;"></div>
-    <div class="row"><?php include 'table.php'; ?></div>
+
+<div class="container" style="margin-top: 50px">
+    <form class="form-inline" action="index.php" method="get">
+        <div class="form-group">
+            <label for="device">Device</label>
+            <select name="device" class="form-control" id="device">
+                <?php $tables = $db->getTables();
+                // ToDo: show last selected device
+                foreach ($tables as $table) echo '<option value="' . $table[0] . '">' . $table[0] . '</option>';
+                ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="start">From</label>
+            <input id="start" name="start" class="form-control" type="datetime-local"
+                   value="<?php echo isset($start) ? $start : _date(7); ?>">
+        </div>
+        <div class="form-group">
+            <label for="end">To</label>
+            <input id="end" name="end" class="form-control" type="datetime-local"
+                   value="<?php echo isset($end) ? $end : _date() ?>">
+        </div>
+        <button type="submit" class="btn btn-primary">Update</button>
+    </form>
+    <div id="graph" class="row" style="height: 1000px;"></div>
+    <div class="row">
+        <table class="table table-hover table-striped">
+            <thead>
+            <tr>
+                <?php $fields = $db->connect()->query($query)->fetch_fields();
+                foreach ($fields as $field) echo '<th>' . ucfirst($field->name) . '</th>';
+                ?>
+            </tr>
+            </thead>
+            <tbody>
+            <?php $db->readCSV($query); ?>
+            </tbody>
+        </table>
+    </div>
 </div>
+
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 <script src="graph.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"
-        integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"
-        integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"
-        integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK"
-        crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"></script>
 </body>
 </html>
